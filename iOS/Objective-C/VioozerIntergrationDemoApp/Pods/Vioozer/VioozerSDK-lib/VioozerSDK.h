c96754c38840fb35e8239713baea2b46f337fa3e
Yedidya Reiss <yedidya@Yedidyas-MacBook-Pro.local>
2017-01-19 13:20:30 +0200

    pod update to Objective-C demo (allowing server request to fail without crash)

--- iOS/Objective-C/VioozerIntergrationDemoApp/Pods/Vioozer/VioozerSDK-lib/VioozerSDK.h
+++ iOS/Objective-C/VioozerIntergrationDemoApp/Pods/Vioozer/VioozerSDK-lib/VioozerSDK.h
@@ -5,8 +5,8 @@
 //  Created by Zeev on 12/1/14.
 //  Copyright (c) 2014 Quickode. All rights reserved.
 //
-// VioozerSDK version 1.5.9
-#define VIOOZER_SDK_VERSION @"1.6.3"
+// VioozerSDK version 1.6.4
+#define VIOOZER_SDK_VERSION @"1.6.4"
 
 #import <Foundation/Foundation.h>
 #import "VioozerLocation.h"
